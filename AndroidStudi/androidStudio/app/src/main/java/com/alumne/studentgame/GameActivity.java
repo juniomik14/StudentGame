fad85735b2436e2463de242224613b3ff08d4454
juniomik14 <juniomik14@gmail.com>
2017-11-30 21:10:39 +0100

    Creacion del icono Student Game

--- AndroidStudi/androidStudio/app/src/main/java/com/alumne/studentgame/GameActivity.java
+++ AndroidStudi/androidStudio/app/src/main/java/com/alumne/studentgame/GameActivity.java
@@ -3,11 +3,11 @@ package com.alumne.studentgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class StudenGAME extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_studen_game);
+        setContentView(R.layout.activity_game);
     }
 }
---
+++ AndroidStudi/androidStudio/app/src/main/java/com/alumne/studentgame/activitys/StudenGAME.java
@@ -0,0 +1,15 @@
+package com.alumne.studentgame.activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alumne.studentgame.R;
+
+public class StudenGAME extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_studen_game);
+    }
+}
